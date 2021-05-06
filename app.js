47d805b7e03bb6c4d342d9ce8f3fbe086aeb3e94
JayashreeChandrasekaran <72458955+JayashreeChandrasekaran@users.noreply.github.com>
2021-05-06 15:28:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const hamberger=document.querySelector('.hamberger-icns');
+const box=document.querySelector('.navb-cont');
+
+
+hamberger.addEventListener('click',()=>{
+
+    box.classList.toggle('show');
+
+});
